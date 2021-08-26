02788506d018f3462e62051e2d3adcbea197b2e0
Tim H <timh1203@gmail.com>
2021-08-25 22:39:20 -0500

    Add rng and comparison

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    // Generate a random number.
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
     println!("Guess the number!");
-
+    println!("Secret number: {}", secret_number);
     println!("Please input your guess.");
 
     // Capture the input.
@@ -10,12 +15,25 @@ fn main() {
     // new is an associated function.
     let mut guess = String::new();
 
-    // Read the input.
+    // Read the input and saves as string to variable guess.
     // Bring in libary to read the line and handle Ok or Error.
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Need to convert guess number into a number.
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number.");
+
     // Print the input.
     println!("You guessed: {}", guess);
+
+    // Compare the secret number and guess.
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low."),
+        Ordering::Equal => println!("You win."),
+        Ordering::Greater => println!("Too high."),
+    };
 }
