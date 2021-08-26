5a5c0748ee332d5fbde2dac53752ec61f27652a2
Tim H <timh1203@gmail.com>
2021-08-25 22:20:34 -0500

    Add input capture and return

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // Capture the input.
+    // String is of the owned type and always UTF-8.
+    // new is an associated function.
+    let mut guess = String::new();
+
+    // Read the input.
+    // Bring in libary to read the line and handle Ok or Error.
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // Print the input.
+    println!("You guessed: {}", guess);
 }
